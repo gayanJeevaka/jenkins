3656e56f8e71c1d0782c3298b61731b620973fb4
gayanjeevaka <gayan.j@myskylit.com>
2022-04-05 12:57:08 +0530

    Add Test logger

--- src/test/java/com/gayan/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/gayan/jenkins/JenkinsApplicationTests.java
@@ -1,6 +1,8 @@
 package com.gayan.jenkins;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,8 @@ class JenkinsApplicationTests {
 
     @Test
     void contextLoads() {
+        Logger log = LoggerFactory.getLogger(this.getClass());
+        log.info("Welcome to jenkins build");
     }
 
 }
